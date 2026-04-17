975b0d7bb80cf52672d2a45ae697e18047a54cf6
Bharath <bharathVemuri7@gmail.com>
2026-04-17 00:07:05 -0400

    Client-Side

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* <Navbar/>
+      <GameDisplay/>
+      <div>
+        <ChatSidebar/>
+      </div> */}
+    </>
+  )
+}
+
+export default App
