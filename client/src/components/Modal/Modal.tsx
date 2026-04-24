a119283d899a4ebc7a1a396ddb1c5c7ff4420cda
Bharath <bharathVemuri7@gmail.com>
2026-04-23 22:06:16 -0400

    making rooms

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -5,24 +5,51 @@ function Modal({ startGame, socket }: { startGame: (mode: string) => void; socke
     const [showPlayerRoom, setShowPlayerRoom] = useState(false);
     const [nickname, setNickname] = useState('');
     const [roomCode, setRoomCode] = useState('');
+    const [joinCode, setJoinCode] = useState('');
     const [playersInRoom, setPlayersInRoom] = useState<string[]>([]);
+    const [mode, setMode] = useState(true); // true ==> create && false ==> join
 
     const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNickname(e.target.value);
     };
 
     const navigateToPlayerRoom = () => {
-        socket.emit('createRoom', { nickname }, (response: any) => {
-            setRoomCode(response.roomCode);
-            setPlayersInRoom(response.players);
-        });
-
         setShowPlayerRoom(true);
+        createRoom();
+    }
+
+    const createRoom = () => {
+        if (!roomCode) {
+            socket.emit('createRoom', { nickname }, (response: any) => {
+                console.log("Response:", response);
+                setRoomCode(response.roomId);
+            });
+        }
+    }
+
+    const handleJoinCodeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setJoinCode(e.target.value);
+    }
+
+    const copyOrConnect = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        
+        if (mode) { // copy uuid url
+            await navigator.clipboard.writeText(roomCode);
+        } else { // join room
+            socket.emit('joinRoom', { nickname, roomId:joinCode }, (response: any) => {
+                //TODO: update player roster
+                console.log("Response:", response);
+            });
+        }
     }
 
     const goBack = () => {
-        setShowPlayerRoom(false);
+        setRoomCode('');
+        setJoinCode('');
+        setMode(true);
         setPlayersInRoom([]);
+        setShowPlayerRoom(false);
+
     }
 
     const kickPlayer = (index: number) => {
@@ -65,15 +92,48 @@ function Modal({ startGame, socket }: { startGame: (mode: string) => void; socke
                 ) : (
                     <div className="setup-pg-2">
                         <div className="modal-content mb-4">
-                            <div className="room-link">
-                                <input
-                                    disabled
-                                    value={roomCode}
-                                    className="border border-gray-300 bg-gray-200 color-black-800 rounded-l py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <button className="px-4 py-2 bg-blue-500 text-white rounded-r hover:bg-blue-600" onClick={() => navigator.clipboard.writeText(roomCode)}>
-                                    copy
-                                </button>
+
+                            <div className="connection-container">
+                                <div className="flex border-b">
+                                    <button
+                                        onClick={() => setMode(true)}
+                                        className={`px-1 text-sm rounded-tl w-1/2 ${!mode
+                                            ? "border-b border-black border-r font-medium border-l border-t border-gray-300 text-white"
+                                            : "text-gray-600 bg-gray-300 "
+                                            }`}
+                                    >
+                                        create
+                                    </button>
+
+                                    <button
+                                        onClick={() => setMode(false)}
+                                        className={`px-1 text-sm rounded-tr w-1/2 ${mode
+                                            ? "border-b border-black border-r font-medium border-r border-t border-gray-300 text-white"
+                                            : "text-gray-600 bg-gray-300 "
+                                            }`}
+                                    >
+                                        join
+                                    </button>
+                                </div>
+                                <div className="room-link">
+                                    {mode ? (
+                                        <input
+                                            disabled
+                                            value={roomCode}
+                                            onChange={handleJoinCodeInput}
+                                            className="w-4/5 border border-gray-300 bg-gray-200 color-black-800 rounded-bl py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        />
+                                    ) : (
+                                        <input
+                                            value={joinCode}
+                                            onChange={handleJoinCodeInput}
+                                            className="w-4/5 border border-gray-300 bg-gray-200 color-black-800 rounded-bl py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        />
+                                    )}
+                                    <button className="w-1/5 px-4 py-2 bg-blue-500 text-white rounded-br hover:bg-blue-600" onClick={copyOrConnect}>
+                                        icon
+                                    </button>
+                                </div>
                             </div>
                             <div className="room-container bg-gray-200 mt-4 w-full h-48 rounded-lg p-4">
                                 <ul>
